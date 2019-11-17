68ddc1f5aa093b4fb9964b2b1402075c737e72e0
Yasin Dahi <ymdahi@gmail.com>
2019-11-16 20:38:56 -0500

    use select label as options group

--- js/edtalks-theme.js
+++ js/edtalks-theme.js
@@ -5,7 +5,12 @@
     Drupal.behaviors.visualScripts = {
       attach: function (context, settings) {
 
-        
+        // use select element label as option heading
+        $('.form-type-select').each(function(){
+          var selectLabel = $('.control-label', this).text().css('display','none');
+          var selectEl = $('.select-wrapper > select', this);
+          $(selectEl).innerWrap(`<optgroup label="${selectLabel}></optgroup>`)
+        });
 
 
       }
